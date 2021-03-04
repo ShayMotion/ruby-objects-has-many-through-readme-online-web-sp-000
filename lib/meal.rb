a6d19cd374bc4d6df51b178cdbaf8faa9e87003f
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-04 20:10:06 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,9 +1,7 @@
-class Meal
- 
   attr_accessor :waiter, :customer, :total, :tip
- 
+
   @@all = []
- 
+
   def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
@@ -11,8 +9,8 @@ class Meal
     @tip = tip
     @@all << self
   end
- 
+
   def self.all
     @@all
   end
-end
+end 
